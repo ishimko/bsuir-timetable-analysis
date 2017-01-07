90a59ce5193d43a53ab9b828fd471096e292b036
Ivan <vana.shimko@gmail.com>
2017-01-07 13:06:39 +0300

    Use python's default argument parser

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-import sys
-from helper import fatal_error, press_enter
+import argparse
+from helper import press_enter
 from downloader import download_timetable
 from busyness_info_builder import build_auditoriums_busyness, write_result
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        fatal_error("Недостаточно параметров!")
+    argument_parser = argparse.ArgumentParser(description="Building BSUIR auditoriums' busyness info")
+    argument_parser.add_argument('cache_path', type=str, help='path to the cache for the loading timetable')
+    argument_parser.add_argument('output_path', type=str, help='path to the output file')
+    args = argument_parser.parse_args()
 
-    timetable_cache_path = sys.argv[1]
-    result_file_path = sys.argv[2]
-
-    download_timetable(timetable_cache_path)
-    write_result(build_auditoriums_busyness(timetable_cache_path), result_file_path)
+    download_timetable(args.cache_path)
+    write_result(build_auditoriums_busyness(args.cache_path), args.output_path)
 
     press_enter()
