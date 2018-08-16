ce15a3e38da1dd5b46f3a504f3ca9679cd11b2fb
Ivan Shimko <vana.shimko@gmail.com>
2018-08-16 10:44:10 +0300

    Add default values for --output and --action

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # BSUIR timetable analysis util
 
 ```
-usage: main.py [-h] [--skip-check] [--action ACTION] cache-path output_path
+usage: main.py [-h] [--output OUTPUT] [--skip-check] [--action ACTION]
+               cache-path
 
 positional arguments:
   cache-path       path to the cache of a timetable
-  output_path      path to the output file
 
 optional arguments:
   -h, --help       show this help message and exit
+  --output OUTPUT  path to the output file, default is <action>.json
   --skip-check     skip loading a timetable, use cache
-  --action ACTION  script to run against built info
+  --action ACTION  script to run against built info, default is "represent"
 ```
--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from busyness_info_builder import build_auditoriums_busyness
 from importlib import import_module
 
 
-def execute_action(busyness, action_name, output_path):
+DEFAULT_ACTION_NAME = 'represent'
+
+
+def execute_action(busyness, action_name):
     action_module = import_module(action_name)
     action_func = getattr(action_module, 'action')
     result = action_func(busyness)
@@ -16,7 +19,6 @@ def execute_action(busyness, action_name, output_path):
 
 def write_result(obj, result_file):
     try:
-        log_info('Writing results\n')
         f = open(result_file, 'w', encoding='utf-8')
         f.write(json.dumps(obj, ensure_ascii=False, indent=4))
     except OSError as e:
@@ -29,20 +31,20 @@ def main(args):
         download_timetable(args.cache_path)
     timetable_db = shelve.open(args.cache_path, writeback=True)
     result = build_auditoriums_busyness(timetable_db)
-    if args.action:
-        result = execute_action(result, args.action, args.output_path)
-    else:
-        result = result
+    action_name = args.action if args.action else DEFAULT_ACTION_NAME
+    output_path = args.output if args.output else f'{action_name}.json'
     log_info('Executing action')
-    write_result(result, args.output_path)
+    result = execute_action(result, action_name)
+    log_info('Writing results')
+    write_result(result, output_path)
 
 
 if __name__ == '__main__':
     argument_parser = argparse.ArgumentParser(description="BSUIR timetable analysis util")
     argument_parser.add_argument('cache_path', metavar='cache-path', type=str, help='path to the cache of a timetable')
-    argument_parser.add_argument('output_path', metavar='output_path', type=str, help='path to the output file')
+    argument_parser.add_argument('--output', type=str, help='path to the output file, default is <action>.json')
     argument_parser.add_argument('--skip-check', action='store_true', help='skip loading a timetable, use cache')
-    argument_parser.add_argument('--action', type=str, help='script to run against built info')
+    argument_parser.add_argument('--action', type=str, help='script to run against built info, default is "represent"')
 
     args = argument_parser.parse_args()
     main(args)
